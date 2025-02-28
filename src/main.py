f10d881539bfd9290e048e7e761b3fddbe312b27
vorozhkog <vorozhkog@icloud.com>
2025-02-28 15:16:29 +0000

    move state

--- src/main.py
+++ src/main.py
@@ -7,11 +7,7 @@ colormap = colormaps[colormap_select.get_value()]
 apply_processing = need_processing.is_on()
 
 app = WebPyApplication(layout)
-state = app.state
-if hasattr(state, "imagePixelsData") is False:
-    setattr(state, "imagePixelsData", None)
-if hasattr(state, "imagePixelsDataImageId") is False:
-    setattr(state, "imagePixelsDataImageId", 0)
+
 
 def process_img(img, colormap):
     img_bgr = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
@@ -25,7 +21,7 @@ def process_img(img, colormap):
 def colormap_changed(value):
     global colormap
     colormap = colormaps[value]
-    new_img = process_img(state.imagePixelsData, colormap)
+    new_img = process_img(app.state.imagePixelsData, colormap)
     app.replace_current_image(new_img)
 
 
@@ -41,6 +37,11 @@ def processing_switched(is_switched):
 def main():
     global colormap
     global apply_processing
+    state = app.state
+    if hasattr(state, "imagePixelsData") is False:
+        setattr(state, "imagePixelsData", None)
+    if hasattr(state, "imagePixelsDataImageId") is False:
+        setattr(state, "imagePixelsDataImageId", 0)
     if apply_processing and state.imagePixelsDataImageId != app.get_current_image_id():
         state.imagePixelsData = app.get_current_image()
         state.imagePixelsDataImageId = app.get_current_image_id()
