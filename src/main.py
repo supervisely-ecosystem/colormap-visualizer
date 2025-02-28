88af204e2d81ee0ab1cf67ccf4a538d38efe7465
vorozhkog <vorozhkog@icloud.com>
2025-02-28 12:28:44 +0000

    wip

--- src/main.py
+++ src/main.py
@@ -4,6 +4,7 @@ from sly_sdk.webpy import WebPyApplication
 from src.gui import layout, need_processing, colormap_select, colormaps
 
 app = WebPyApplication(layout)
+colormap = colormaps[colormap_select.get_value()]
 
 def process_img(img, colormap):
     img_bgr = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
@@ -15,25 +16,23 @@ def process_img(img, colormap):
 
 @colormap_select.value_changed
 def colormap_changed(value):
+    global colormap
     colormap = colormaps[value]
-    img_data = getattr(app.state, "imagePixelsData", app.get_current_image())
-    new_img = process_img(img_data, colormap)
-    app.replace_current_image(new_img)
 
 
 @app.run_function
 def main():
+    global colormap
     if hasattr(app.state, "imagePixelsData") is False:
         setattr(app.state, "imagePixelsData", app.get_current_image())
     if hasattr(app.state, "imagePixelsDataImageId") is False:
-        setattr(app.state, "imagePixelsDataImageId", 0)
+        setattr(app.state, "imagePixelsDataImageId", app.get_current_image_id())
     if not need_processing.is_on():
         app.replace_current_image(app.state.imagePixelsData)
         return
 
-    if app.state.imagePixelsDataImageId != app._context.imageId:
+    if app.state.imagePixelsDataImageId != app.get_current_image_id():
         app.state.imagePixelsData = app.get_current_image()
-        app.state.imagePixelsDataImageId = app._context.imageId
-    colormap = colormaps[colormap_select.get_value()]
+        app.state.imagePixelsDataImageId = app.get_current_image_id()
     new_img = process_img(app.state.imagePixelsData, colormap)
     app.replace_current_image(new_img)
