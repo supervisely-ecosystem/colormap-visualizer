ee5946ea013a1288ed30937af3865bccfa86f1ac
vorozhkog <vorozhkog@icloud.com>
2025-02-28 15:22:44 +0000

    wip

--- src/main.py
+++ src/main.py
@@ -29,6 +29,8 @@ def colormap_changed(value):
 def processing_switched(is_switched):
     if is_switched is False:
         app.replace_current_image(app.state.imagePixelsData)
+    else:
+        app.state.imagePixelsDataImageId = 0
     global apply_processing
     apply_processing = is_switched
 
