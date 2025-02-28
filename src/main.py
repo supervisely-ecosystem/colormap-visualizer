b7907ad30da75d22c16117783ea98d1b9448e860
vorozhkog <vorozhkog@icloud.com>
2025-02-28 15:14:21 +0000

    improve processing logic to avoid redundant processing

--- src/main.py
+++ src/main.py
@@ -3,8 +3,15 @@ import numpy as np
 from sly_sdk.webpy import WebPyApplication
 from src.gui import layout, need_processing, colormap_select, colormaps
 
-app = WebPyApplication(layout)
 colormap = colormaps[colormap_select.get_value()]
+apply_processing = need_processing.is_on()
+
+app = WebPyApplication(layout)
+state = app.state
+if hasattr(state, "imagePixelsData") is False:
+    setattr(state, "imagePixelsData", None)
+if hasattr(state, "imagePixelsDataImageId") is False:
+    setattr(state, "imagePixelsDataImageId", 0)
 
 def process_img(img, colormap):
     img_bgr = cv2.cvtColor(img, cv2.COLOR_RGBA2BGR)
@@ -18,21 +25,24 @@ def process_img(img, colormap):
 def colormap_changed(value):
     global colormap
     colormap = colormaps[value]
+    new_img = process_img(state.imagePixelsData, colormap)
+    app.replace_current_image(new_img)
+
+
+@need_processing.value_changed
+def processing_switched(is_switched):
+    if is_switched is False:
+        app.replace_current_image(app.get_current_image())
+    global apply_processing
+    apply_processing = is_switched
 
 
 @app.run_function
 def main():
     global colormap
-    if hasattr(app.state, "imagePixelsData") is False:
-        setattr(app.state, "imagePixelsData", app.get_current_image())
-    if hasattr(app.state, "imagePixelsDataImageId") is False:
-        setattr(app.state, "imagePixelsDataImageId", app.get_current_image_id())
-    if not need_processing.is_on():
-        app.replace_current_image(app.state.imagePixelsData)
-        return
-
-    if app.state.imagePixelsDataImageId != app.get_current_image_id():
-        app.state.imagePixelsData = app.get_current_image()
-        app.state.imagePixelsDataImageId = app.get_current_image_id()
-    new_img = process_img(app.state.imagePixelsData, colormap)
-    app.replace_current_image(new_img)
+    global apply_processing
+    if apply_processing and state.imagePixelsDataImageId != app.get_current_image_id():
+        state.imagePixelsData = app.get_current_image()
+        state.imagePixelsDataImageId = app.get_current_image_id()
+        new_img = process_img(state.imagePixelsData, colormap)
+        app.replace_current_image(new_img)
